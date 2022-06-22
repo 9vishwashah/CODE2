2a4d81a7262ca99fb21ab0c03a7efeb4e432d6c3
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-22 08:41:24 +0530

    Create swap.cpp
    This is a program that swaps two number.

---
+++ swap.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, temp;
+    cout<< "Enter First Number:\n";
+    cin >> a;
+    cout<< "Enter Second Number:\n";
+    cin >> b;
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+    return 0;
+}
