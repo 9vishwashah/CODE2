46d4006ba9e3058db4f56488ae1cd7c5866f4abb
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-22 19:00:55 +0530

    Create swap_reference.cpp
    This is a program that Swaps Two Value by using Call by reference.

---
+++ swap_reference.cpp
@@ -0,0 +1,21 @@
+#include<iostream>  
+using namespace std;    
+void swap(int *x, int *y)  
+{  
+ int swap;  
+ swap=*x;  
+ *x=*y;  
+ *y=swap;  
+}  
+int main()   
+{    
+ int a,b;
+ cout<<"Enter First Number:\n";
+ cin>>a;
+ cout<<"Enter Second Number:\n";
+ cin>>b;
+ swap(&a, &b);  
+ cout<<"Value of x is: "<<a<<endl;  
+ cout<<"Value of y is: "<<b<<endl;  
+ return 0;  
+}    
