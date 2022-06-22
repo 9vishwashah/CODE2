c877af4c98a624aada60ab630621dd55c5a23f40
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-22 18:56:50 +0530

    Create factorial.cpp
    This is a program that calculates Factorial of entered number.

---
+++ factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>  
+using namespace std;    
+int main()  
+{  
+int fact(int);  
+int f,v;  
+cout<<"Enter any number: ";  
+cin>>v;  
+f=fact(v);  
+cout<<"Factorial of " << v << " is: "<<f<<endl;  
+return 0;  
+}  
+int fact(int n)  
+{  
+if(n<0)  
+return(-1);     
+if(n==0)  
+return(1);  
+else  
+{  
+return(n*fact(n-1));      
+}  
+}  
