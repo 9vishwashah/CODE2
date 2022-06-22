7790c91d87fc1f3e47c9d5f74451f9942d1d39c0
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-22 19:13:49 +0530

    Create vote.cpp
    This is a program that prints whether the Person is eligible to vote or not using Go To .

---
+++ vote.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+ineligible:    
+  cout<<"You are not eligible to vote!\n";    
+  cout<<"Enter your age:\n";    
+int a;  
+cin>>a;  
+  if (a < 18)
+  {    
+    goto ineligible;    
+  }    
+  else    
+  {    
+    cout<<"You are eligible to vote!";     
+  }         
+}  
