1f1e2e153ac8fd304743aeac1cafd15be5861090
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-06-25 16:40:55 +0530

    Create specaial_character.cpp
    This is a program that checks whether the string entered by the user has special character or not.

---
+++ specaial_character.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+void run(string a)
+{
+	regex sc("[@_!#$%^&*()<>?/|}{~:]");
+	if(regex_search(a, sc) == 0)
+		cout << "String is accepted";
+	else
+		cout << "String is not accepted.";
+}
+int main()
+{
+	string a;
+cout << "Enter the string: ";
+cin >> a; 
+	run(a);
+	return 0;
+}
