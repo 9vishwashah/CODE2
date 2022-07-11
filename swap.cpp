90d94f19660b5e36beeb1e8100bb12b028b8fdea
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 08:37:29 +0530

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main()
 {
     int a, b, temp;
-    cout<< "Enter First Number:\n";
-    cin >> a;
-    cout<< "Enter Second Number:\n";
-    cin >> b;
-    cout << "Before swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
+        cout<< "Enter First Number:\n";
+        cin >> a;
+        cout<< "Enter Second Number:\n";
+        cin >> b;
+        cout << "Before swapping." << endl;
+        cout << "a = " << a << ", b = " << b << endl;
     temp = a;
     a = b;
     b = temp;
-    cout << "\nAfter swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
-    return 0;
+        cout << "\nAfter swapping." << endl;
+        cout << "a = " << a << ", b = " << b << endl;
+return 0;
 }
