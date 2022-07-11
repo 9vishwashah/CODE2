a6bd5e98a727ed4e9f0e5197e43af53c67e854de
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 18:46:53 +0530

    Add files via upload

---
+++ subtract.java
@@ -0,0 +1,10 @@
+public class subtract 
+{
+    public static void main(String[]args)
+    { 
+        int a=5;
+        int b=20;
+        int c=a-b;
+        System.out.println(c);
+    }  
+}    
