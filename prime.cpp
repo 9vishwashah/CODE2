ed2915e8abf9531c0bd4bbbdafea1aa4c4238001
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 08:32:43 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -6,21 +6,21 @@ int main()
   bool is_prime = true;
   cout << "Enter a positive integer: ";
   cin >> n;
-  if (n == 0 || n == 1) 
-  {
-    is_prime = false;
-  }
-  for (i = 2; i <= n/2; ++i) 
-  {
-    if (n % i == 0) 
+    if (n == 0 || n == 1) 
+      {
+        is_prime = false;
+      }
+    for (i = 2; i <= n/2; ++i) 
     {
-      is_prime = false;
-      break;
+      if (n % i == 0) 
+        {
+          is_prime = false;
+          break;
+        }
     }
-  }
-  if (is_prime)
-    cout << n << " is a prime number";
-  else
-    cout << n << " is not a prime number";
-  return 0;
+    if (is_prime)
+      cout << n << " is a prime number";
+    else
+      cout << n << " is not a prime number";
+return 0;
 }
