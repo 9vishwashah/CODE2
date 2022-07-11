e2a2901ccb1c8b0f5d469ac22e6bf7339bdfde7b
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 08:39:31 +0530

    Update swap_reference.cpp

--- swap_reference.cpp
+++ swap_reference.cpp
@@ -10,12 +10,12 @@ void swap(int *x, int *y)
 int main()   
 {    
  int a,b;
- cout<<"Enter First Number:\n";
- cin>>a;
- cout<<"Enter Second Number:\n";
- cin>>b;
+    cout<<"Enter First Number:\n";
+    cin>>a;
+    cout<<"Enter Second Number:\n";
+    cin>>b;
  swap(&a, &b);  
- cout<<"Value of x is: "<<a<<endl;  
- cout<<"Value of y is: "<<b<<endl;  
- return 0;  
+    cout<<"Value of x is: "<<a<<endl;  
+    cout<<"Value of y is: "<<b<<endl;  
+return 0;  
 }    
