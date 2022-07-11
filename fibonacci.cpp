7f62ac1fb415833fed21debae3091dec794cf1c5
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 08:12:44 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int main() 
 {  
   int n1=0,n2=1,n3,i,number;    
-    cout<<"Enter the number of elements: ";    
-    cin>>number;    
-    cout<<n1<<" "<<n2<<" ";     
-  for(i=2;i<number;++i)  
- {    
-  n3=n1+n2;    
-  cout<<n3<<" ";    
-  n1=n2;    
-  n2=n3;    
- }    
-   return 0;  
+  cout<<"Enter the number of elements: ";    
+  cin>>number;    
+  cout<<n1<<" "<<n2<<" ";     
+    for(i=2;i<number;++i)  
+      {    
+        n3=n1+n2;    
+        cout<<n3<<" ";    
+        n1=n2;    
+        n2=n3;    
+      }    
+return 0;  
 }  
