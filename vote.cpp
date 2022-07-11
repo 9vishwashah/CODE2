59718dde49cd73d8de3738f6ee198b1d42b344ff
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 08:40:49 +0530

    Update vote.cpp

--- vote.cpp
+++ vote.cpp
@@ -2,17 +2,17 @@
 using namespace std;  
 int main()  
 {  
-ineligible:    
-  cout<<"You are not eligible to vote!\n";    
-  cout<<"Enter your age:\n";    
-int a;  
-cin>>a;  
-  if (a < 18)
-  {    
-    goto ineligible;    
-  }    
-  else    
-  {    
-    cout<<"You are eligible to vote!";     
-  }         
+  ineligible:    
+    cout<<"You are not eligible to vote!\n";    
+    cout<<"Enter your age:\n";    
+  int a;  
+    cin>>a;  
+      if (a < 18)
+        {    
+            goto ineligible;    
+        }    
+      else    
+        {    
+            cout<<"You are eligible to vote!";     
+        }         
 }  
