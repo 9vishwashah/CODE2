5accb2c0d29d2424c91df8e61d1fe0a013470776
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 19:39:37 +0530

    Add files via upload

---
+++ add_num.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class add_num {
+    public static void main(String[]args)
+    {
+        Scanner sc =new Scanner (System.in);
+        System.out.println("Enter First Number");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number");
+        int b = sc.nextInt();
+        int c= a+b;
+        System.out.print("The Sum of Two Number is:");
+        System.out.println(c);
+    }
+}
