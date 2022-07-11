37d218df3729686ac776dcde546a5b3846713520
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 08:09:29 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -2,22 +2,22 @@
 using namespace std;    
 int main()  
 {  
-int fact(int);  
-int f,v;  
-cout<<"Enter any number: ";  
-cin>>v;  
-f=fact(v);  
-cout<<"Factorial of " << v << " is: "<<f<<endl;  
+  int fact(int);  
+  int f,v;  
+  cout<<"Enter any number: ";  
+  cin>>v;  
+  f=fact(v);  
+  cout<<"Factorial of " << v << " is: "<<f<<endl;  
 return 0;  
 }  
 int fact(int n)  
 {  
-if(n<0)  
-return(-1);     
-if(n==0)  
-return(1);  
-else  
-{  
-return(n*fact(n-1));      
-}  
+  if(n<0)  
+      return(-1);     
+  if(n==0)  
+      return(1);  
+  else  
+    {  
+      return(n*fact(n-1));      
+    }  
 }  
