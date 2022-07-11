54a452f25f6bec78345c3b64aaa524ab012e277e
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 08:36:25 +0530

    Update specaial_character.cpp

--- specaial_character.cpp
+++ specaial_character.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 void run(string a)
 {
-	regex sc("[@_!#$%^&*()<>?/|}{~:]");
-	if(regex_search(a, sc) == 0)
-		cout << "String is accepted";
+    regex sc("[@_!#$%^&*()<>?/|}{~:]");
+        if(regex_search(a, sc) == 0)
+	    cout << "String is accepted";
 	else
-		cout << "String is not accepted.";
+            cout << "String is not accepted.";
 }
 int main()
 {
-	string a;
-cout << "Enter the string: ";
-cin >> a; 
-	run(a);
-	return 0;
+    string a;
+    cout << "Enter the string: ";
+    cin >> a; 
+    run(a);
+return 0;
 }
