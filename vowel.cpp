937ee87025ed91e1b66b106c7d8da8c85f68f43d
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-11 08:42:08 +0530

    Update vowel.cpp

--- vowel.cpp
+++ vowel.cpp
@@ -4,15 +4,15 @@ int main()
 {
     char c;
     bool l, u;
-    cout << "Enter an alphabet: ";
-    cin >> c;
+        cout << "Enter an alphabet: ";
+        cin >> c;
     l = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
     u = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-    if (!isalpha(c))
-      printf("Error! Non-alphabetic character.");
-    else if (l || u)
-        cout << c << " is a vowel.";
-    else
-        cout << c << " is a consonant.";
-    return 0;
+        if (!isalpha(c))
+            printf("Error! Non-alphabetic character.");
+        else if (l || u)
+            cout << c << " is a vowel.";
+        else
+            cout << c << " is a consonant.";
+return 0;
 }
