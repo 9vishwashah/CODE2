a415901f365ce437a78b656e7a5df2dbd5c035e5
VISHWA ALPESH SHAH <104209238+9vishwashah@users.noreply.github.com>
2022-07-16 08:49:55 +0530

    Create url_check.java

---
+++ url_check.java
@@ -0,0 +1,54 @@
+// Java program to check URL is valid or not
+// using Regular Expression
+import java.util.*;
+import java.util.regex.*;
+
+class Main {
+
+	// Function to validate URL
+	// using regular expression
+	public static boolean
+	isValidURL(String url)
+	{
+    
+		// Regex to check valid URL
+		String regex = "((http|https)://)(www.)?"
+			+ "[a-zA-Z0-9@:%._\\+~#?&//=]"
+			+ "{2,256}\\.[a-z]"
+			+ "{2,6}\\b([-a-zA-Z0-9@:%"
+			+ "._\\+~#?&//=]*)";
+
+		// Compile the ReGex
+		Pattern p = Pattern.compile(regex);
+
+		// If the string is empty
+		// return false
+		if (url == null) {
+			return false;
+		}
+
+		// Find match between given string
+		// and regular expression
+		// using Pattern.matcher()
+		Matcher m = p.matcher(url);
+
+		// Return if the string
+		// matched the ReGex
+		return m.matches();
+	}
+
+	// Driver code
+	public static void main(String args[])
+	{
+        try(Scanner sc=new Scanner(System.in))
+        {
+        System.out.print("Enter a string: ");  
+        String url= sc.nextLine();    
+        }
+		if (isValidURL(url) == true) {
+			System.out.println("Yes");
+		}
+		else
+			System.out.println("NO");
+	}
+}
